081da57d8b16bf130296aefab3713006b9e6e192
Jackson Humphrey <jackson.s.humphrey@gmail.com>
2022-01-18 13:04:03 -0600

    Removing the extraneous config header objects.

--- f1db.py
+++ f1db.py
@@ -12,23 +12,6 @@ import f1db_udfs # This file defines all user-defined functions to compile at co
 logging.basicConfig(level = logging.NOTSET)
 logger = logging.getLogger("f1db")
 
-DATABASE_FILE_NAME = "f1.db"
-
-CSV_FILES_DIR = "./raw_data_files"
-ERGAST_DOWNLOAD_URL = "http://ergast.com/downloads/f1db_csv.zip"
-ERGAST_ZIP_FILE_NAME = "f1db.zip"
-
-SQL_SCRIPT_FILES_DIR = "./sql_scripts"
-TABLE_DEFINITION_SCRIPT_FILE = "define_base_tables.sql" # SQL script file that defines the DB tables
-RELOAD_SCRIPT_FILES = [ # List of SQL script files that get run when the DB is reloaded from scratch
-    "extended_base_tables.sql",
-    "retirements.sql",
-    "lap_positions.sql", # Depends on retirements
-    "overtakes.sql" # Depends on lap_positions and retirements
-]
-
-CONSOLE_OUTPUT_ROW_LIMIT = 20 # Defines the maximum number of rows dumped out to the console
-
 class Query:
 
     def __init__(self, name, connection, output_table_name, sql_script_file_name):
@@ -48,8 +31,6 @@ class QueryVisualization:
         self.query = query
 
         # self.figure = pass
-
-
 
 
 class Connection:
